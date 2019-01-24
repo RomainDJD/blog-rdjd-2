aef5f6fb215d3b6e247ad306ff686da81661de34
RomainDJD <44061192+RomainDJD@users.noreply.github.com>
2019-01-24 09:50:25 +0100

    Update user.controller.ts

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -7,11 +7,6 @@ import { UserService } from './user.service';
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Get(':id')
-  async getById(@Param('id') id: string) {
-    return this.userService.getById(id);
-  }
-
   @Delete(':id')
   async deleteById(@Param('id') id: string) {
     return this.userService.deleteUser(id);
