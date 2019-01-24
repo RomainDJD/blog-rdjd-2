3203fe8f7a4314e681dc4ff10daf84a004fc483c
RomainDJD <44061192+RomainDJD@users.noreply.github.com>
2019-01-24 12:15:06 +0100

    create table

---
+++ src/article/entity/article.entity.ts
@@ -0,0 +1,40 @@
+import {
+    Column,
+    CreateDateColumn,
+    Entity,
+    PrimaryGeneratedColumn,
+    UpdateDateColumn,
+    OneToOne,
+    JoinColumn,
+  } from 'typeorm';
+  
+  @Entity()
+  export class Article {
+  
+    @CreateDateColumn()
+    created: Date;
+
+    @User_author()
+    created: Date;
+  
+    @Column({ type: 'varchar', name: 'titre', length: 200 })
+    titre: string;
+  
+    @Column({ type: 'varchar', name: 'article_content', length: 100 })
+    articleContent: string;
+    
+    @UpdateDateColumn()
+    updated: Date;
+
+    @PrimaryGeneratedColumn('uuid', { name: 'user_id' })
+    articleId: string;
+
+    @OneToOne(type => userId)
+    @JoinColumn()
+    user_Id: userId;
+
+   //  @ManyToOne(type => Category)
+ //@JoinColumn() this decorator is optional for @ManyToOne, but required for @OneToOne
+ //category: Category;
+  }
+  
