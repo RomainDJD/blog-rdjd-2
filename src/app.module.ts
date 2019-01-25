c3b0177bef7f7ebba0b329a874a3cc51a0ee5b1d
RomainDJD <44061192+RomainDJD@users.noreply.github.com>
2019-01-25 19:47:05 +0100

    25

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { ArticleService } from './article/article.service';
+import { ArticleModule } from './article/article.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, ArticleModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ArticleService],
 })
 export class AppModule {}
