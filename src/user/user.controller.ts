4985cb0d7b6e1610c8cb03b34791eea1a1ba0dc4
RomainDJD <44061192+RomainDJD@users.noreply.github.com>
2019-03-10 13:31:53 +0100

    HttpStatus

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post, Put, Delete } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put, Delete, HttpStatus } from '@nestjs/common';
 import { ApiResponse, ApiUseTags } from '@nestjs/swagger';
 import { User } from './entity/user.entity';
 import { UserService } from './user.service';
@@ -12,8 +12,21 @@ import { UserService } from './user.service';
 export class UserController {
   constructor(private readonly userService: UserService) {}
   @Get(':id')
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'User trouvé et retourné',
+  })
+  @ApiResponse({
+    status: HttpStatus.NOT_FOUND,
+    description: 'User non trouvé :/',
+  })
   async getById(@Param('id') id: string) {
     return this.userService.getById(id);
+  }
+
+  @Delete(':id')
+  async deleteById(@Param('id') id: string) {
+    return this.userService.deleteUser(id);
   }
   @Put(':id')
   async modifyById(@Param('id') id: string, @Body() dto: Partial<User>) {
